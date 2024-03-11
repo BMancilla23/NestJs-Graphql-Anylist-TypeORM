9cf86e786277c0fc93cd61e921ae2b2b55cb2028
bmancilla23 <bmancilla119@gmail.com>
2024-03-11 07:52:57 -0500

    Conexión a la base de datos

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,6 +5,7 @@ import { join } from 'path';
 import { ApolloServerPluginLandingPageLocalDefault} from '@apollo/server/plugin/landingPage/default';
 import { ItemsModule } from './items/items.module';
 import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
   imports: [
@@ -17,6 +18,17 @@ import { ConfigModule } from '@nestjs/config';
         ApolloServerPluginLandingPageLocalDefault()
       ]
     }),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.DB_HOST,
+      port: +process.env.DB_PORT,
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
+      synchronize: true,
+      autoLoadEntities: true
+    })
+    ,
     ItemsModule
   ],
   controllers: [],
